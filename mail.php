505a2ccd22b44acbd49c98a7888a6c3c0d4df369
Rxssett <67123290+Rxssette543@users.noreply.github.com>
2020-08-22 21:36:26 -0500

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,9 +1,22 @@
 <?php
-    $destino= rossetteismael@gmail.com;
-    $nombre = $_POST["nombre"];
-    $correo = $_POST["correo"];
-    $mensaje = $_POST["mensaje"];
-    $contenido = "Nombre: " . $nombre . "\nCorreo" . $correo . "Mensaje" . $mensaje;
-    mail($destino, "Gaceta" ,$contenido);
-    header("location:tanks.html")
+$nombre = $_POST['nombre'];
+$mail = $_POST['correo'];
+$empresa = $_POST['mensaje'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'rossetteismael@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+mail($para, $asunto, utf8_decode($mensaje), $header);
+
+header("Location:tanks.html");
 ?>
